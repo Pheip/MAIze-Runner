143ba661daf3e1ad28750609fa5357c3c5fd49ea
PHEIPP\Philipp Heise <heisephilipp2@web.de>
2021-08-25 11:18:22 +0200

    Refactoring of the code, if escaped, a new maze will be generated

--- Maize-Runner/Assets/Scripts/EscapeMazeAgent.cs
+++ Maize-Runner/Assets/Scripts/EscapeMazeAgent.cs
@@ -10,17 +10,16 @@ public class EscapeMazeAgent : Agent
     public float moveSpeed = 4f;
     bool check = true;
     Vector3 PlayerPosition = Vector3.zero;
-    Player player;
     [SerializeField] private Transform targetTransform;
     public Material winMaterial;
     Material defaultMaterial;
+    
     public override void OnEpisodeBegin()
     {
         
         if (check)
         {
-            player = this.GetComponent<Player>();
- 
+             
             GameObject go = gameObject;
             Debug.Log(go.transform.position);
             PlayerPosition = go.transform.localPosition;
@@ -29,29 +28,33 @@ public class EscapeMazeAgent : Agent
             defaultMaterial = gameObject.GetComponent<MeshRenderer>().material;
         }
       
-        if (!check)
+        else
         {
-            transform.localPosition = placeGameFigure(player.xOffset,player.yOffset,player);
+            LearningInstance li = this.transform.parent.GetComponent<LearningInstance>();
+            li.createNewMaze();
+            
+            transform.localPosition = placeGameFigure();
             
         }
         
        
     }
 
-    private Vector3 placeGameFigure(int xOffset, int yOffset, Player players)
+    private Vector3 placeGameFigure()
     {
-        int maxX = player.maze.GetUpperBound(1);
-        int maxY = player.maze.GetUpperBound(0);
+        InstanceInformation info = this.transform.parent.GetComponent<InstanceInformation>();
+        int maxX = info.maze.GetUpperBound(1);
+        int maxY = info.maze.GetUpperBound(0);
         int x = 0;
         int y = 0;
         do
         {
             x = Random.Range(maxX - 5, maxX - 1);
             y = Random.Range(0, maxY - 1);
-        } while (players.maze[x, y] == 1);
-        Debug.Log("Player instantiate at: " + (x + xOffset) + " " + (y + yOffset));
+        } while (info.maze[x, y] == 1);
+        Debug.Log("Player instantiate at: " + (x) + " " + (y));
 
-        return new Vector3(x + xOffset, 0.5f, y + yOffset);
+        return new Vector3(x, 0.5f, y);
    
     }
 
@@ -62,7 +65,7 @@ public class EscapeMazeAgent : Agent
         float moveZ = input[1];
        // Debug.Log(vectorAction[0]);
         transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
-        AddReward(+0.001f);
+        AddReward(-.1f);
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -84,19 +87,19 @@ public class EscapeMazeAgent : Agent
     {
         if (other.TryGetComponent<Goal>(out Goal goal))
         {
-            AddReward(+10);
-            //SetReward(+10f);
+            
+           SetReward(+10f);
             Debug.Log("Finish");
             gameObject.GetComponent<MeshRenderer>().material = winMaterial;
             EndEpisode();
         }
 
-        if (other.TryGetComponent<Wall>(out Wall wall))
+     /*   if (other.TryGetComponent<Wall>(out Wall wall))
         {
             SetReward(-1f);
             gameObject.GetComponent<MeshRenderer>().material = defaultMaterial;
             EndEpisode();
-        }
+        }*/
 
     }
 }
